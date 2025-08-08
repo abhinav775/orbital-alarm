fed0e93ffe012636f94ccfd8d3cad7b58ea9fe0e
abhinav775 <abhinavgopalmb@gmail.com>
2025-08-08 19:40:50 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const earth = document.getElementById("earth");
+const orbit = document.getElementById("orbit");
+const alarmSound = document.getElementById("alarmSound");
+const alarmTimeDisplay = document.getElementById("alarmTime");
+let isDragging = false;
+let alarmHour = null;
+let alarmMinute = null;
