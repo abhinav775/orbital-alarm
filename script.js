764435b9d2963b9a7aac4e06a93f4a553aecedef
abhinav775 <abhinavgopalmb@gmail.com>
2025-08-08 19:54:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ const alarmTimeDisplay = document.getElementById("alarmTime");
 let isDragging = false;
 let alarmHour = null;
 let alarmMinute = null;
+orbit.addEventListener("mousedown", (e) => {
+  isDragging = true;
+});
+document.addEventListener("mouseup", () => {
+  isDragging = false;
+});
